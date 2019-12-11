766c09d2e03e8d78bc2ff20f1ebf13ec81f12787
Yhargrim <58047675+Yhargrim@users.noreply.github.com>
2019-12-11 14:02:28 -0800

    Cleaned up repository

--- src/dungeon/src/dungeon/Sorceress.java
+++
@@ -1,8 +0,0 @@
-package dungeon;
-
-public class Sorceress extends Hero {
-
-	public Sorceress(AttackFactory attackFactory) {
-		super("Sorceress", 75, 5, "Fireball", .3, attackFactory, "Healing Word");
-	}  
-}
